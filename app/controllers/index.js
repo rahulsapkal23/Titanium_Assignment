e3710b748b7b1fa96090b5e5a03430b7c571be44
rahulsapkal23 <rahul.sapkal@neosofttech.com>
2016-11-02 20:13:19 +0530

    assign 3 static

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,27 @@
+var rowData=[];
+for (var i = 1; i <=10; i++)
+{
+  var payload1={
+                  rowId:i,
+                  itemName:'Test'+i
+                }
+  var row1=Alloy.createController('row123',payload1).getView();
+//  Ti.API.info(row1);
+  rowData.push(row1);
+}
+
+
 function showItems123(e) {
+  //  Ti.API.info(JSON.stringify(e));
   var payload={
-    rowId:"11",
-    itemName:"ad"
+    rowId:e.rowData.rowId,
+    itemName:e.rowData.itemName
   }
   var w=Alloy.createController('detailwin',payload).getView();
   w.open();
 
 }
 
-
-$.itemsList123.data="rowData";
+//Ti.API.info(rowData);
+$.itemsList123.data=rowData;
 $.win123.open();
---
+++ app/controllers/row123.js
@@ -0,0 +1,4 @@
+var args =arguments[0] || {};
+//  Ti.API.info(JSON.stringify(args));
+$.itemsList12=args.rowId;
+$.label12.text=args.itemName;
